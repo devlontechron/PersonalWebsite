d24f7dc2cd997db2a1fca979c6f3edc8e566e205
Devin Durham <devindurham95@gmail.com>
2016-10-25 01:36:32 -0700

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -3,15 +3,15 @@ window.onload = function(){
 
 var imgArray = [];
 
-imgArray[0] = "\"../media/pictures/WP_20130817_181.jpg"\";
-imgArray[1] = "\"../media/pictures/WP_20140701_17_57_52_Pro.jpg"\" ;
-imgArray[2] = "\"../media/pictures/WP_20140814_013.jpg"\";
-imgArray[3] = "\"../media/pictures/WP_20150702_21_21_14_Pro (2).jpg"\" ;
-imgArray[4] = "\"../media/pictures/WP_20150725_12_45_10_Pro.jpg"\" ;
-imgArray[5] = "../media/pictures/WP_20160522_15_17_47_Pro_LI (3).jpg"\" ;
-imgArray[6] = "../media/pictures/WP_20160625_21_30_37_Pro (2).jpg"\" ;
-imgArray[7] = "../media/pictures/WP_20160913_14_42_22_Pro.jpg"\" ;
-imgArray[8] = "../media/pictures/tumblr_mnhlogNdIK1ssuam5o1_1280.jpg"\" ;
+imgArray[0] = "\"../media/pictures/WP_20130817_181.jpg\"";
+imgArray[1] = "\"../media/pictures/WP_20140701_17_57_52_Pro.jpg\"" ;
+imgArray[2] = "\"../media/pictures/WP_20140814_013.jpg\"";
+imgArray[3] = "\"../media/pictures/WP_20150702_21_21_14_Pro (2).jpg\"" ;
+imgArray[4] = "\"../media/pictures/WP_20150725_12_45_10_Pro.jpg\"" ;
+imgArray[5] = "../media/pictures/WP_20160522_15_17_47_Pro_LI (3).jpg\"" ;
+imgArray[6] = "../media/pictures/WP_20160625_21_30_37_Pro (2).jpg\"" ;
+imgArray[7] = "../media/pictures/WP_20160913_14_42_22_Pro.jpg\"" ;
+imgArray[8] = "../media/pictures/tumblr_mnhlogNdIK1ssuam5o1_1280.jpg\"" ;
     
     
 var rando =Math.floor(Math.random()*9);
