e503231f3b801d61b5c25ead39fe673509883d1a
Devin Durham <devindurham95@gmail.com>
2016-10-25 01:05:41 -0700

    Update background.js

--- JS/background.js
+++ JS/background.js
@@ -3,7 +3,7 @@ window.onload = function(){
 
 var imgArray = [];
 
-imgArray[0] = "../media/pictures/WP_20130817_181.jpg";
+imgArray[0] = "'../media/pictures/WP_20130817_181.jpg'";
 imgArray[1] = "../media/pictures/WP_20140701_17_57_52_Pro.jpg" ;
 imgArray[2] = "../media/pictures/WP_20140814_013.jpgg";
 imgArray[3] = "../media/pictures/WP_20150702_21_21_14_Pro (2).jpg" ;
@@ -17,7 +17,7 @@ imgArray[8] = "../media/pictures/tumblr_mnhlogNdIK1ssuam5o1_1280.jpg" ;
 var rando =Math.floor(Math.random()*9);
     var x = imgArray[rando];
   
-     document.body.style.background = "#f3f3f3 url(x) no-repeat center fixed";
+     document.body.style.background = "#f3f3f3 url(imgArray[0]) no-repeat center fixed";
 
 
 };
