1e4a60f08c98a3962c41f490fbc704f835c57a4b
Devin Durham <devindurham95@gmail.com>
2016-10-24 20:58:12 -0700

    Update and rename test.html to test.js

---
+++ test.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+    document.getElementById("header").innerHTML = "Lorem ipsum dolor sit amet, consetetur   sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+};
