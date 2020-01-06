006722d55c3c00fa906afe6f1e0df9a0843b1e67
DevlonTecron <DevinDurham95@live.com>
2020-01-06 15:07:37 -0800

    Json formatting for functions

--- getUsers/__init__.py
+++ getUsers/__init__.py
@@ -1,12 +1,14 @@
 import logging
 import pyodbc
+import json
 
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('GetUsers function starting request')
-    responseData =""
+    
+    responseData = dict()
 
     #PLEASE STARILIZE YOUR SQL
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:holidaze.database.windows.net,1433;Database=users;Uid=devlontecron;Pwd=LaSalle!0;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
@@ -14,21 +16,32 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     cursor.execute("SELECT * FROM dbo.phrases")
     data = cursor.fetchall()
 
-    logging.info(data)
-    logging.info(responseData)
+    phraseDic = {}
+    phraseList = []
+    for row in data:
+        currPhrase = {}
+        currPhrase["phrase"] =row.phrase
+        currPhrase["count"] =row.count    
 
-    responseData += str(data)
+        phraseList.append(currPhrase)
+    
+    responseData["phrases"] = phraseList 
 
     conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Server=tcp:holidaze.database.windows.net,1433;Database=users;Uid=devlontecron;Pwd=LaSalle!0;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM dbo.users")
     data = cursor.fetchall()
+    
+    userList = []
+    for row in data:
+        currUser = {}
+        currUser["name"] =row.name
+        currUser["score"] =row.score   
+        
+        userList.append(currUser)  
 
-    logging.info(data)
-    logging.info(responseData)
+    responseData["users"] = userList
 
-    responseData += str(data)
-
-    return func.HttpResponse(responseData, 
+    return func.HttpResponse(str(json.dumps(responseData)), 
         status_code=200
         )
